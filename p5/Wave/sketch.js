8f47cfcd01f1c3614b4fa7ee665dd04812b88bce
Timi <timi.ogunkeye@gmail.com>
2022-09-28 20:21:33 +0100

    extra colour maps, colour shifting effect

--- p5/Wave/sketch.js
+++ p5/Wave/sketch.js
@@ -4,6 +4,9 @@ let r_offset=255;
 let g_offset=1255;
 let b_offset=2255;
 
+let r_offset2=255;
+let g_offset2=1255;
+let b_offset2=2255;
 setup = () => {
   //Waves setup
   for (let i = 0; i < 5; i++) {
@@ -27,13 +30,21 @@ setup = () => {
     let noiseMap_b=map(noise(b_offset), 0, 1, 0, 255);
     background(noiseMap_r, noiseMap_g, noiseMap_b);
 
+
+
     //Waves example
     for (let x = 0; x < width; x += 10) {
+      r_offset2+=random(0.000001);
+      g_offset2+=random(0.000001);
+      b_offset2+=random(0.000001);
+      let r=map(noise(r_offset2), 0, 1, 0, 255);
+      let g=map(noise(g_offset2), 0, 1, 0, 255);
+      let b=map(noise(b_offset2), 0, 1, 0, 255);
+      
       let y = 0;
       waves.forEach(wave =>  y+= wave.evaluate(x));
-      waves.forEach((wave,i) => fill(colours[i].r, colours[i].g, colours[i].b));
       noStroke();
-      
+      fill(r,g,b);
       ellipse(x, y + height / 2, 16);
     }
 
